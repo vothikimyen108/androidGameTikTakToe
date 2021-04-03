1ecfae109f2c23841976e5b285ae9d3819bf3f48
vothikimyen108 <vothikinyen2k@gmail.com>
2021-04-03 13:32:38 +0700

    update ui

---
+++ app/src/main/java/com/example/vothikimyen_1851050194_bai7/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vothikimyen_1851050194_bai7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView d;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
